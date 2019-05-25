b58deee0c796cc8d507cc138a3f235d48b2ed326
Blazh <iblazevi@gmail.com>
2019-05-25 22:24:31 +0200

    Add sound on hover

--- src/app/open-source/libraries/library-item/library-item.component.ts
+++ src/app/open-source/libraries/library-item/library-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Howl } from 'howler';
 import { Library } from '../library.model';
 
 @Component({
@@ -9,4 +10,13 @@ import { Library } from '../library.model';
 export class LibraryItemComponent {
   @Input()
   data: Library;
+
+  private sound = new Howl({
+    src: ['assets/sounds/ui_tap-variant-01.wav']
+  });
+
+  onHover(): void {
+    this.sound.play();
+  }
+
 }
