743e207af2cc868046bacf96c09410081823895d
Blazh <iblazevi@gmail.com>
2019-05-28 11:38:32 +0200

    Fixes

--- src/app/open-source/libraries/libraries.component.ts
+++ src/app/open-source/libraries/libraries.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LIBRARIES } from './data';
 import { Library } from './library.model';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-libraries',
@@ -8,5 +10,17 @@ import { Library } from './library.model';
   styleUrls: ['./libraries.component.scss']
 })
 export class LibrariesComponent {
+
+  isTabSwitched: boolean = false;
+
   items: Library[] = LIBRARIES;
+
+  constructor(private router: Router) {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(e => {
+      this.isTabSwitched = e["url"] != '/open-source';
+    });
+  }
+
 }
