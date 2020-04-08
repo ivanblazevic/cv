01f64ac0f91503f7b14138298aa672b907e9b8d0
Ivan Blazevic <ivan.blazevic@gmail.com>
2020-04-08 14:40:22 +0200

    Initial commit

--- src/app/experience/experience.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ExperienceItem {
-    title: string;
-    date: string;
-    logo: string;
-    site: string;
-    items: string[];
-}
--- src/app/open-source/libraries/library.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Library {
-  link: string;
-  title: string;
-  description: string;
-}
